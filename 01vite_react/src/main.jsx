9d42a18c090cd2a9dae4f0cb4a232b172c55b079
Shreshtha-ai <shreshthaagarwal2007@gmail.com>
2026-06-15 23:13:22 +0530

    feat: initialize main.jsx with MyApp component and render configuration

--- 01vite_react/src/main.jsx
+++ 01vite_react/src/main.jsx
@@ -2,8 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return (
+    <h1>Hello World</h1>
+  )
+}
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Visit Google'
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target = '_blank'>Visit Google</a>
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+    
+    <MyApp/>
+
+  
 )
