6ccccb1f321689ddfd503ae819ee32a28b63bbb6
Shreshtha-ai <shreshthaagarwal2007@gmail.com>
2026-06-16 19:48:30 +0530

    feat: implement counter component with increment and decrement functionality and basic styling

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,8 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
-
 function App() {
 
   let [counter, setCounter] = useState(15)
