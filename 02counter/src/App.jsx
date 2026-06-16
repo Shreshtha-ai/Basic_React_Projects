cde1f6f18fb08a093422820ae1fef33c35b5cb25
Shreshtha-ai <shreshthaagarwal2007@gmail.com>
2026-06-16 19:03:01 +0530

    feat: initialize basic react counter application with state management

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(15)
+
+  // let counter = 5
+
+  function addValue(){
+    counter = counter +1;
+    setCounter(counter)
+    console.log("Value added", counter);
+  }
+  function removeValue(){
+    counter = counter -1;
+    setCounter(counter)
+    console.log("Value removed", counter);                 
+  }
+
+  return (
+   <>                    
+   
+   <h1> React Counter </h1>
+   <h2> Counter value:{counter}</h2>
+   <button style={{padding: '10px 20px', alignSelf: 'center'}} onClick={addValue}> Add value  </button>
+   <br></br>
+   <button style={{padding: '10px 20px', alignSelf: 'center'}} onClick={removeValue}> Remove value  </button>
+   </>
+  )
+}
+
+export default App
