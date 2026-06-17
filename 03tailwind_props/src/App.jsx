9bcd54306bec25117d07bd3ff58156c37cd0b720
Shreshtha-ai <shreshthaagarwal2007@gmail.com>
2026-06-17 17:47:25 +0530

    feat: initialize React project with Tailwind CSS configuration and Card component structure

---
+++ 03tailwind_props/src/App.jsx
@@ -0,0 +1,21 @@
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <>
+   <h1 className='bg-green-400 text-black p-4 rounded-xl text-2xl'> Tailwind Test</h1>
+   <Card username="chaiaurcode"/>
+   <Card username = "Shreshtha Agarwal"/>
+   <Card/>
+  
+
+
+  
+  
+
+   </>
+  )
+}
+
+export default App
